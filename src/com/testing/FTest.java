ab7dcec4ae9dfe813dd69c5db8db42766dff95fb
MadalinMatasaru <madalin.matasaru@yahoo.com>
2016-10-14 00:39:55 -0700

    Add files via upload

--- src/com/testing/FTest.java
+++ src/com/testing/FTest.java
@@ -1,43 +1,44 @@
 package com.testing;
 
-import java.awt.Image;
-import java.awt.Label;
-import java.awt.TextField;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import javax.imageio.ImageIO;
-import javax.swing.text.LabelView;
 
 import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.rules.Timeout;
 
 import unittesting.UnitTestClassBase;
 
-import com.google.common.collect.Table.Cell;
-import com.hp.lft.report.Reporter;
-import com.hp.lft.sdk.*;
-import com.hp.lft.sdk.java.*;
-import com.hp.lft.sdk.sap.gui.LabelDescription;
+import com.hp.lft.sdk.Desktop;
+import com.hp.lft.sdk.GeneralLeanFtException;
+import com.hp.lft.sdk.MouseButton;
+import com.hp.lft.sdk.RegExpProperty;
+import com.hp.lft.sdk.java.Button;
+import com.hp.lft.sdk.java.Dialog;
+import com.hp.lft.sdk.java.DialogDescription;
+import com.hp.lft.sdk.java.Editor;
+import com.hp.lft.sdk.java.List;
+import com.hp.lft.sdk.java.ListDescription;
+import com.hp.lft.sdk.java.Menu;
+import com.hp.lft.sdk.java.MenuDescription;
+import com.hp.lft.sdk.java.Table;
+import com.hp.lft.sdk.java.TableCell;
+import com.hp.lft.sdk.java.TableDescription;
+import com.hp.lft.sdk.java.TableRow;
+import com.hp.lft.sdk.java.TreeView;
+import com.hp.lft.sdk.java.TreeViewDescription;
+import com.hp.lft.sdk.java.UiObject;
+import com.hp.lft.sdk.java.UiObjectDescription;
+import com.hp.lft.sdk.java.Window;
 import com.hp.lft.sdk.stdwin.EditorDescription;
-import com.hp.lft.sdk.web.ButtonDescription;
-import com.hp.lft.sdk.web.XPathDescription;
-import com.hp.lft.sdk.winforms.WindowDescription;
-import com.hp.lft.verifications.Verify;
-import com.hp.lft.sdk.insight.*;
-
-import java.awt.image.*;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Date;
-import java.util.Properties;
-
-import static org.junit.Assert.*;
+import com.utils.TableUtils;
 
 public class FTest extends UnitTestClassBase {
 	Properties prop = new Properties();
@@ -47,7 +48,14 @@ public class FTest extends UnitTestClassBase {
 				String server;
 				String user;
 				String password;
-		
+	
+//	JsonParser parser = new JsonParser();
+//	Object obj = parser.parse(new FileReader("C:\\Users\\mapt\\Desktop\\Test.txt"));
+//	JsonObject jsonObject = (JsonObject) obj;
+//	String server = jsonObject.get("server").toString();
+//	String user = jsonObject.get("user").toString(); 
+//	String password = jsonObject.get("password").toString();
+	
 	public FTest() {
 		//Change this constructor to private if you supply your own public constructor
 	}
@@ -59,8 +67,7 @@ public class FTest extends UnitTestClassBase {
 		
 		Runtime.getRuntime().exec("C:\\Program Files (x86)\\HPE SA\\launcher\\hpe_sa_launcher.exe");
 	}
-	
-	
+
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 		globalTearDown();
@@ -93,7 +100,7 @@ public class FTest extends UnitTestClassBase {
          try {Thread.sleep(20000);} catch (InterruptedException e) {e.printStackTrace();}
 	}
 	@Test
-	public void test() throws GeneralLeanFtException {
+	public void test() throws Exception {
 		 //Select Library and then Import Software form the Actions menu      
 		
 		Window afterLoginWindow =  Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
@@ -167,7 +174,7 @@ public class FTest extends UnitTestClassBase {
 			.selectedNodes(new String[] { "#0;Application Configuration" }).build())
 			.select("#0;Package");
          
-         //enter 'test' in the search field and select the test policy 
+         //enter 'test' in teh search field and select the test policy 
          testPolicyWindow.describe(Editor.class, new com.hp.lft.sdk.java.EditorDescription.Builder()
 			.tagName("JTextField").nativeClass("javax.swing.JTextField").build()).setText("test");  
          testPolicyWindow.describe(Table.class, new TableDescription.Builder()
@@ -183,7 +190,6 @@ public class FTest extends UnitTestClassBase {
 			.label("Devices").build()).click();
          afterLoginWindow.describe(TreeView.class, new TreeViewDescription.Builder()
 			.attachedText("Devices").build()).select("Devices;Servers;All Managed Servers");
-     
          afterLoginWindow.describe(Table.class, new TableDescription.Builder()
 			.objectName("server-table").build()).selectRows(0); 
         
@@ -195,34 +201,43 @@ public class FTest extends UnitTestClassBase {
          afterLoginWindow.describe(Menu.class, new MenuDescription.Builder()
 			.label("Actions").build()).selectSubMenu("Attach;Software Policy...");
          try {Thread.sleep(5000);} catch (InterruptedException e) {e.printStackTrace();}
-
+		 
+		 
 		 Table table =Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
 			.title("HPE Server Automation - 192.168.178.30").build()).describe(Dialog.class, new DialogDescription.Builder()
 			.title("Attach Software Policy").build()).describe(Table.class, new TableDescription());
 		
-				 
-		 for (TableRow row:table.getRows()){
-			 for (TableCell cell:row.getCells() ){
-				
-				 if (cell.getValue().toString().equals("Test Policy")) {
-					 cell.click();
-					 
-				}
-				
-			 }
-		 }
-		
-		 
+		 TableUtils.searchTableDoubleClick(table, "Test Policy");	 
+         
          afterLoginWindow.describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
 			.label("Attach").build()).click();
          Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
 			.title("Remediate").build()).describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
 			.label("Start Job").build()).click();
          try {Thread.sleep(15000);} catch (InterruptedException e) {e.printStackTrace();}
-       
-         UiObject tabel = Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-			.title(new RegExpProperty("Remediate.*")).build()).describe(UiObject.class, new UiObjectDescription.Builder()
-			.nativeClass("com.opsware.ngui.apppolicy.task.RemediatePoliciesTask$2").build());
+       //  Verify.areEqual("Succeedeed", )
+//         UiObjectDescription test = new UiObjectDescription.Builder().attachedText("Job Status")
+//        		 .nativeClass("com.opsware.ngui.apppolicy.task.RemediatePoliciesTask$2").build();
+        
+    
+         
+//         UiObject tabel = Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+//			.title(new RegExpProperty("Remediate.*")).build()).describe(UiObject.class, new UiObjectDescription.Builder()
+//			.nativeClass("com.opsware.ngui.apppolicy.task.RemediatePoliciesTask$2").build());
+//         
+
+//         //String bitmapFolder = ("@C:\\Users\\mapt\\Desktop\\Images\\Reference1.bmp");
+//        		 try {
+//					RenderedImage expectedImage = ImageIO.read(new File("C:\\Users\\mapt\\Desktop\\Images\\Reference1.PNG"));
+//					if (tabel.verifyImageMatch(expectedImage))
+//						System.out.println("BINGO!!!!");
+//							;
+//				} catch (IOException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+        		 
+         
          
          Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
 			.title(new RegExpProperty("Remediate.*")).build()).describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
@@ -236,29 +251,23 @@ public class FTest extends UnitTestClassBase {
 		
 		Table servers = afterLoginWindow.describe(Table.class, new TableDescription.Builder()
 		.objectName("server-table").build()); 
-        for (TableRow serverrows:servers.getRows()){
-		 for (TableCell servercell:serverrows.getCells() ){
-			 //System.out.println(servercell.getValue().toString());
-			 if (servercell.getValue().toString().equals("tv-355.ta.opsware.com")) {
-				 servercell.doubleClick();
-				 Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder().title("Server: tv-355.ta.opsware.com").build())
-				 .describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder().label("Management Policies").build()).click();
-				 Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-					.title("Server: tv-355.ta.opsware.com").build()).describe(TreeView.class, new TreeViewDescription.Builder()
-					.attachedText("Management Policies").build()).select("Management Policies;Software Policies");	
-			}
-			 
-		 }
-	 }
-     
+		
+		TableUtils.searchTableDoubleClick(servers,"tv-355.ta.opsware.com");
+		try {Thread.sleep(3000);} catch (InterruptedException e) {e.printStackTrace();}
+		
+		Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder().title("Server: tv-355.ta.opsware.com").build())
+		 .describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder().label("Management Policies").build()).click();
+		 Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+			.title("Server: tv-355.ta.opsware.com").build()).describe(TreeView.class, new TreeViewDescription.Builder()
+			.attachedText("Management Policies").build()).select("Management Policies;Software Policies");	
+		 try {Thread.sleep(3000);} catch (InterruptedException e) {e.printStackTrace();}
+		 
      Table Policies = Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
 		.title("Server: tv-355.ta.opsware.com").build()).describe(Table.class, new TableDescription.Builder()
 		.attachedText("Software Policies").build());
-     for (TableRow Policiesrows:Policies.getRows()){
-		 for (TableCell Policiescell:Policiesrows.getCells() ){
-			 if (Policiescell.getValue().toString().equals("Test Policy")) {
-				 Policiescell.click(MouseButton.RIGHT);
-				 
+     
+     TableUtils.searchTableRightClick(Policies,"Test Policy");
+                
 				 Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
 					.title("Server: tv-355.ta.opsware.com").build()).describe(Menu.class, new MenuDescription.Builder()
 					.label("Actions").build()).click();
@@ -274,16 +283,11 @@ public class FTest extends UnitTestClassBase {
 					try {Thread.sleep(20000);} catch (InterruptedException e) {e.printStackTrace();}
 					Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
 					.title(new RegExpProperty("Uninstall Software.*")).build()).describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
-					.label("Close").build()).click();
-			 }
-			 }
-		 }
-   
+					.label("Close").build()).click();			
         
 		afterLoginWindow.close();
 		afterLoginWindow.describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
 		.label("Yes").build()).click();	
 	}
 }
- 
  
---
+++ src/com/utils/TableUtils.java
@@ -0,0 +1,43 @@
+package com.utils;
+
+import com.hp.lft.sdk.Desktop;
+import com.hp.lft.sdk.MouseButton;
+import com.hp.lft.sdk.java.Button;
+import com.hp.lft.sdk.java.Table;
+import com.hp.lft.sdk.java.TableCell;
+import com.hp.lft.sdk.java.TableDescription;
+import com.hp.lft.sdk.java.TableRow;
+import com.hp.lft.sdk.java.TreeView;
+import com.hp.lft.sdk.java.TreeViewDescription;
+import com.hp.lft.sdk.java.Window;
+
+public class TableUtils {
+
+public static void searchTableDoubleClick(Table servers, String searchString) throws Exception
+{
+    for (TableRow serverrows:servers.getRows()){
+	 for (TableCell servercell:serverrows.getCells() ){
+		 System.out.println(servercell.getValue().toString());
+		 if (servercell.getValue().toString().equals(searchString)) {
+			 servercell.doubleClick();
+		}
+		 else  System.out.println("Not Found");
+	 }
+    }
+
+}
+
+public static void searchTableRightClick(Table servers, String searchString) throws Exception
+{
+    for (TableRow serverrows:servers.getRows()){
+	 for (TableCell servercell:serverrows.getCells() ){
+		 System.out.println(servercell.getValue().toString());
+		 if (servercell.getValue().toString().equals(searchString)) {
+			 servercell.click(MouseButton.RIGHT);
+		}
+		 else  System.out.println("Not Found");
+	 }
+    }
+
+}
+}
