5d60a654ae3eea254892dd5858a04a1c39489cbd
MadalinMatasaru <madalin.matasaru@yahoo.com>
2016-10-18 00:14:44 -0700

    Add files via upload

--- src/com/testing/LeanFtTest.java
+++ src/com/testing/LeanFtTest.java
@@ -78,93 +78,93 @@ public class LeanFtTest extends UnitTestClassBase {
 			.title("HPE Server Automation Client Login").build()).describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
 			.label("Log In").build()).click();
         try {Thread.sleep(20000);} catch (InterruptedException e) {e.printStackTrace();}
-
-        //Select Library and then Import Software form the Actions menu         
-        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-			.title("HPE Server Automation - 192.168.178.30").build()).describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
-			.label("Library").build()).click();
-        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-			.title("HPE Server Automation - 192.168.178.30").build()).describe(TreeView.class, new TreeViewDescription.Builder()
-			.attachedText("Library").build()).select("By Type;Packages;Red Hat;Red Hat Enterprise Linux Server 5");
-        try {Thread.sleep(5000);} catch (InterruptedException e) {e.printStackTrace();}  
-        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-			.title("HPE Server Automation - 192.168.178.30").build()).describe(Menu.class, new MenuDescription.Builder()
-			.label("Actions").build()).click();
-        try {Thread.sleep(5000);} catch (InterruptedException e) {e.printStackTrace();}
-        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-			.title("HPE Server Automation - 192.168.178.30").build()).describe(Menu.class, new MenuDescription.Builder()
-			.label("Actions").build()).selectSubMenu("Import Software...");
-        try {Thread.sleep(5000);} catch (InterruptedException e) {e.printStackTrace();}
-        
-        // Import a dummy .zip file - the Browse buttons are identical so that we need to use Location property to distinguish them
-        // also close all the modal windows that are being opened
-        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-			.title("HPE Server Automation - 192.168.178.30").build()).describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
-			.attachedText("Browse...").location(new com.hp.lft.sdk.LocationProperty().setX(651).setY(7)).build()).click();
-        try {Thread.sleep(5000);} catch (InterruptedException e) {e.printStackTrace();}
-        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-			.title("HPE Server Automation - 192.168.178.30").build()).describe(Dialog.class, new DialogDescription.Builder()
-			.title("Software Imports").build()).describe(Dialog.class, new DialogDescription.Builder()
-			.title("Open").build()).describe(Editor.class, new EditorDescription.Builder()
-			.attachedText("File Name:").build()).setText("test.zip");
-        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-			.title("HPE Server Automation - 192.168.178.30").build()).describe(Dialog.class, new DialogDescription.Builder()
-			.title("Software Imports").build()).describe(Dialog.class, new DialogDescription.Builder()
-			.title("Open").build()).describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
-			.label("Open").build()).click();
-        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-			.title("HPE Server Automation - 192.168.178.30").build()).describe(Dialog.class, new DialogDescription.Builder()
-			.title("Software Imports").build()).describe(Editor.class, new EditorDescription.Builder()
-			.attachedText("Folder:").build()).setText("/");
-        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-			.title("HPE Server Automation - 192.168.178.30").build()).describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
-			.label("Import").build()).click();
-        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-			.title("HPE Server Automation - 192.168.178.30").build()).describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
-			.label("Replace").build()).click();
-        try {Thread.sleep(3000);} catch (InterruptedException e) {e.printStackTrace();}
-        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-			.title("Software Imports").build()).describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
-			.label("Close").build()).click();
-        try {Thread.sleep(3000);} catch (InterruptedException e) {e.printStackTrace();}
-        
-        //Create a new Software Policy - select the Software Policy target from the tree and then Actions - New menu
-        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-			.title("HPE Server Automation - 192.168.178.30").build()).describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
-			.label("Library").build()).click();
-        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-			.title("HPE Server Automation - 192.168.178.30").build()).describe(TreeView.class, new TreeViewDescription.Builder()
-			.attachedText("Library").build()).select("By Type;Software Policies;Red Hat;Red Hat Enterprise Linux Server 5");
-        try {Thread.sleep(3000);} catch (InterruptedException e) {e.printStackTrace();}
-        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-			.title("HPE Server Automation - 192.168.178.30").build()).describe(Menu.class, new MenuDescription.Builder()
-			.label("Actions").build()).click();
-        try {Thread.sleep(5000);} catch (InterruptedException e) {e.printStackTrace();}
-        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-			.title("HPE Server Automation - 192.168.178.30").build()).describe(Menu.class, new MenuDescription.Builder()
-			.label("Actions").build()).selectSubMenu("New...");
-        try {Thread.sleep(3000);} catch (InterruptedException e) {e.printStackTrace();}
-        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-			.title("Software Policy: New Software Policy*").build()).describe(Editor.class, new EditorDescription.Builder()
-			.attachedText("Name:").build()).setText("test policy");
-        try {Thread.sleep(3000);} catch (InterruptedException e) {e.printStackTrace();}
-        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-			.title("Software Policy: test policy*").build()).describe(TreeView.class, new TreeViewDescription.Builder()
-			.attachedText("Views").build()).select("Software Policy:;Policy Items");
-        
-        // press the add button to add the zip file
-        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-			.title("Software Policy: test policy*").build()).describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
-			.label("add16").build()).click();
-        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-			.title("Software Policy: test policy*").build()).describe(TreeView.class, new TreeViewDescription.Builder()
-			.selectedNodes(new String[] { "#0;Application Configuration" }).build())
-			.select("#0;Package");
-        
-        //enter 'test' in teh search field and select the test policy 
-        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
-			.title("Software Policy: test policy*").build()).describe(Editor.class, new com.hp.lft.sdk.java.EditorDescription.Builder()
-			.tagName("JTextField").nativeClass("javax.swing.JTextField").build()).setText("test");  
+//
+//        //Select Library and then Import Software form the Actions menu         
+//        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+//			.title("HPE Server Automation - 192.168.178.30").build()).describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
+//			.label("Library").build()).click();
+//        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+//			.title("HPE Server Automation - 192.168.178.30").build()).describe(TreeView.class, new TreeViewDescription.Builder()
+//			.attachedText("Library").build()).select("By Type;Packages;Red Hat;Red Hat Enterprise Linux Server 5");
+//        try {Thread.sleep(5000);} catch (InterruptedException e) {e.printStackTrace();}  
+//        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+//			.title("HPE Server Automation - 192.168.178.30").build()).describe(Menu.class, new MenuDescription.Builder()
+//			.label("Actions").build()).click();
+//        try {Thread.sleep(5000);} catch (InterruptedException e) {e.printStackTrace();}
+//        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+//			.title("HPE Server Automation - 192.168.178.30").build()).describe(Menu.class, new MenuDescription.Builder()
+//			.label("Actions").build()).selectSubMenu("Import Software...");
+//        try {Thread.sleep(5000);} catch (InterruptedException e) {e.printStackTrace();}
+//        
+//        // Import a dummy .zip file - the Browse buttons are identical so that we need to use Location property to distinguish them
+//        // also close all the modal windows that are being opened
+//        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+//			.title("HPE Server Automation - 192.168.178.30").build()).describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
+//			.attachedText("Browse...").location(new com.hp.lft.sdk.LocationProperty().setX(651).setY(7)).build()).click();
+//        try {Thread.sleep(5000);} catch (InterruptedException e) {e.printStackTrace();}
+//        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+//			.title("HPE Server Automation - 192.168.178.30").build()).describe(Dialog.class, new DialogDescription.Builder()
+//			.title("Software Imports").build()).describe(Dialog.class, new DialogDescription.Builder()
+//			.title("Open").build()).describe(Editor.class, new EditorDescription.Builder()
+//			.attachedText("File Name:").build()).setText("test.zip");
+//        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+//			.title("HPE Server Automation - 192.168.178.30").build()).describe(Dialog.class, new DialogDescription.Builder()
+//			.title("Software Imports").build()).describe(Dialog.class, new DialogDescription.Builder()
+//			.title("Open").build()).describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
+//			.label("Open").build()).click();
+//        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+//			.title("HPE Server Automation - 192.168.178.30").build()).describe(Dialog.class, new DialogDescription.Builder()
+//			.title("Software Imports").build()).describe(Editor.class, new EditorDescription.Builder()
+//			.attachedText("Folder:").build()).setText("/");
+//        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+//			.title("HPE Server Automation - 192.168.178.30").build()).describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
+//			.label("Import").build()).click();
+//        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+//			.title("HPE Server Automation - 192.168.178.30").build()).describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
+//			.label("Replace").build()).click();
+//        try {Thread.sleep(3000);} catch (InterruptedException e) {e.printStackTrace();}
+//        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+//			.title("Software Imports").build()).describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
+//			.label("Close").build()).click();
+//        try {Thread.sleep(3000);} catch (InterruptedException e) {e.printStackTrace();}
+//        
+//        //Create a new Software Policy - select the Software Policy target from the tree and then Actions - New menu
+//        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+//			.title("HPE Server Automation - 192.168.178.30").build()).describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
+//			.label("Library").build()).click();
+//        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+//			.title("HPE Server Automation - 192.168.178.30").build()).describe(TreeView.class, new TreeViewDescription.Builder()
+//			.attachedText("Library").build()).select("By Type;Software Policies;Red Hat;Red Hat Enterprise Linux Server 5");
+//        try {Thread.sleep(3000);} catch (InterruptedException e) {e.printStackTrace();}
+//        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+//			.title("HPE Server Automation - 192.168.178.30").build()).describe(Menu.class, new MenuDescription.Builder()
+//			.label("Actions").build()).click();
+//        try {Thread.sleep(5000);} catch (InterruptedException e) {e.printStackTrace();}
+//        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+//			.title("HPE Server Automation - 192.168.178.30").build()).describe(Menu.class, new MenuDescription.Builder()
+//			.label("Actions").build()).selectSubMenu("New...");
+//        try {Thread.sleep(3000);} catch (InterruptedException e) {e.printStackTrace();}
+//        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+//			.title("Software Policy: New Software Policy*").build()).describe(Editor.class, new EditorDescription.Builder()
+//			.attachedText("Name:").build()).setText("test policy");
+//        try {Thread.sleep(3000);} catch (InterruptedException e) {e.printStackTrace();}
+//        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+//			.title("Software Policy: test policy*").build()).describe(TreeView.class, new TreeViewDescription.Builder()
+//			.attachedText("Views").build()).select("Software Policy:;Policy Items");
+//        
+//        // press the add button to add the zip file
+//        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+//			.title("Software Policy: test policy*").build()).describe(Button.class, new com.hp.lft.sdk.java.ButtonDescription.Builder()
+//			.label("add16").build()).click();
+//        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+//			.title("Software Policy: test policy*").build()).describe(TreeView.class, new TreeViewDescription.Builder()
+//			.selectedNodes(new String[] { "#0;Application Configuration" }).build())
+//			.select("#0;Package");
+//        
+//        //enter 'test' in teh search field and select the test policy 
+//        Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
+//			.title("Software Policy: test policy*").build()).describe(Editor.class, new com.hp.lft.sdk.java.EditorDescription.Builder()
+//			.tagName("JTextField").nativeClass("javax.swing.JTextField").build()).setText("test");  
         Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
 		.title("HPE Server Automation - 192.168.178.30").build()).close();
         Desktop.describe(Window.class, new com.hp.lft.sdk.java.WindowDescription.Builder()
